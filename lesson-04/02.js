8fb356a3b38a9a3f821cfbb5b0c1ecaf1e1e1592
nataliaN <asdfghmax@yandex.ru>
2025-04-20 20:15:35 +0300

    findUniqueElements

--- lesson-04/02.js
+++ lesson-04/02.js
@@ -14,5 +14,21 @@
 
 Подсказка: можно использовать функцию `includesElement`, которую мы написали ранее. Переписывать её не нужно, она доступна по всему проекту за счёт hoisting.
 */
-
-function findUniqueElements() {}
+function includesElement(arr, elem){
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === elem){ return true;}
+    }
+    return false;
+}
+function findUniqueElements(arr$) {
+    let newArr = [];
+    for (let i = 0; i < arr$.length; i++) {
+        if (newArr.length === 0) {
+            newArr.push(arr$[i])
+        } else if (!includesElement(newArr, arr$[i])) {
+            newArr.push(arr$[i]);
+        }
+    }
+    return newArr;
+}
+console.log(findUniqueElements([1, 2, 3, 2, 1, 4, 5, 4, 1, 6, 15]));
