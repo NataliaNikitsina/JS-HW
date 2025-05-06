a1c9d9026cf2f11911d3f961f647101539793ad9
nataliaN <asdfghmax@yandex.ru>
2025-05-06 11:49:04 +0300

    capitalizeWords-two-versions

--- lesson-07/01.js
+++ lesson-07/01.js
@@ -6,15 +6,26 @@
   Другой вариант - использовать метод split строк, и метод массива join.
 */
 
+// вариант 1
+// function capitalizeWords(str) {
+//     str = str.replace(str[0], str[0].toUpperCase())
+//     for (let i = 0; i < str.length; i++) {
+//         if (str[i] === ' ') {
+//             str = str.replace(str[i+1], str[i+1].toUpperCase())
+//         }
+//     }
+//     return str;
+// }
+
+// вариант 2
 function capitalizeWords(str) {
-    str = str.replace(str[0], str[0].toUpperCase())
-    for (let i = 0; i < str.length; i++) {
-        if (str[i] === ' ') {
-            str = str.replace(str[i+1], str[i+1].toUpperCase())
-        }
+    let arr = str.split(' ');
+    for (let i = 0; i < arr.length; i++) {
+       arr[i] = arr[i].replace(arr[i][0], arr[i][0].toUpperCase())
     }
-    return str;
+    return arr.join(' ');
 }
+
 
 console.log(capitalizeWords("hello world from javascript"))
 
