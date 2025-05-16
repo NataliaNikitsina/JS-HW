feb6351c70fe6af97c9827c4bea7d8d34691305d
nataliaN <asdfghmax@yandex.ru>
2025-05-16 20:24:03 +0300

    map

--- lesson-09/02.js
+++ lesson-09/02.js
@@ -24,4 +24,12 @@ const doubledNumbers = map(numbers, (element, index) => {
 console.log(doubledNumbers) // Должен вывести: [2, 4, 6, 8, 10]
 */
 
-const map = () => {}
+const map = (array, callback) => {
+    const newArray = [];
+    for (let j = 0; j < array.length; j++) {
+        newArray.push(callback(array[j], j));
+    }
+    return newArray
+}
+
+
